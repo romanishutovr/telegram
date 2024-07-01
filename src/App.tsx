021222370f06f6cdf6b30c2ebc518105a3632b6b
Roman <romanIshutov1995@gmail.com>
2024-07-01 16:26:07 +0300

    save

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 declare global {
   interface Window {
@@ -9,20 +9,38 @@ declare global {
 const TG = window.Telegram.WebApp;
 
 export const  App = () => {
+  const [count, setCount] = useState<number>(0)
+  const [energy, setEnergy] = useState<number>(10)
+  const onClick = useCallback(() => {
+    if(energy === 0) return
+    setCount(count + 1)
+    setEnergy(energy - 1)
+  },[count, energy])
+
 
   useEffect(() => {
     TG.ready();
   },[])
 
-  const onClose = () => {
-    TG.close();
-  }
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if(energy < 10) {
+        setEnergy(energy + 1)
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [energy]);
 
 
   return (
-    <div className="App">
-      <p onClick={onClose}>close</p>
-      <p>{TG.initDataUnsafe?.user?.name}</p>
+    <div style={{display:"flex", flexDirection:"column", alignItems:"center", gap:"20px", background:"white"}}>
+      <div style={{display:"flex", flexDirection:"row" }}>
+      <p>{TG.initDataUnsafe?.user?.username}</p>
+      <img style={{width:"50px", height:"50px", objectFit:"cover"}} src={TG.initDataUnsafe?.user?.photo_url} alt="" />
+      </div>
+      <p>{count} Yojji coins</p>
+      <p>{energy} / 10 energy</p>
+      <img onClick={onClick}  style={{width:"50px", height:"50px", objectFit:"cover"}}  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7nffFy7ZLnQkFmjKFD8cPi9QeBwtmemhdJQ&s" alt="" />
     </div>
   );
 }
