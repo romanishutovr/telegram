192eccd6557cd11b27fac214ede1af4473d3881b
Roman <romanIshutov1995@gmail.com>
2024-07-11 14:11:16 +0300

    save

--- src/App.tsx
+++ src/App.tsx
@@ -1,168 +1,23 @@
-import { useEffect, useState } from "react";
-// import { useDispatch} from "react-redux";
-// import { addTodos, decrement, increment, removeTodos } from "./store/toolkitSlice";
-// import axios from "axios";
-import { TableContainer, Table, TableHead, TableRow, TableCell, TableBody, Box } from "@mui/material";
-// or
-import { CircularProgress } from "@mui/material";
-import ccxt from "ccxt";
-// (async function () {
-//   let kraken    = new ccxt.kraken ()
-// console.log (kraken, await kraken.fetchTickers())
-// }) ()
+import { Box, Button, Stack } from "@mui/material";
+import { useEffect } from "react";
+
 declare global {
   interface Window {
     Telegram: any;
   }
 }
 
-// const TG = window.Telegram.WebApp;
+const TG = window.Telegram.WebApp;
 
 export const App = () => {
-  // const count = useSelector((state:any) => state.toolkit.count)
-  // const todos = useSelector((state:any) => state.toolkit.todos)
-  // const [newTodoName, setNewTodoName] = useState<string>("2313")
-
-  // const dispatch = useDispatch()
-  // const q = apiClient.get("https://pro-api.coinmarketcap.com/v1/exchange/market-pairs/latest")
-  const [state, setState] = useState<any>(null);
-  const [tableState, setTableState] = useState<any>([]);
-  const [loader, setLoader] = useState<boolean>(true);
-  let kraken = new ccxt.kraken();
-  let binance = new ccxt.binance();
-  let whitebit = new ccxt.whitebit();
-
-  const getData = async () => {
-    setLoader(true);
-    const krakenData = await kraken.fetchTickers();
-    const binanceData = await binance.fetchTickers();
-    const whitebitData = await whitebit.fetchTickers();
-
-    const filterDataByUSDT = (data: any) => {
-      const result: any = {};
-
-      for (const exchange in data) {
-        result[exchange] = {};
-        for (const pair in data[exchange]) {
-          if (data[exchange].hasOwnProperty(pair) && pair.includes("/USDT")) {
-            result[exchange][pair] = data[exchange][pair];
-          }
-        }
-      }
-
-      return result;
-    };
-
-    setState(filterDataByUSDT({ krakenData, binanceData, whitebitData }));
-  };
-
   useEffect(() => {
-    getData();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    TG.expand();
   }, []);
 
-  useEffect(() => {
-    if (!state) return;
-    const symbols = Object.keys(state.binanceData);
-
-    const uniqueSymbols = [...new Set(symbols)];
-
-    const result = uniqueSymbols.map((symbol) => {
-      const prices = [state.binanceData[symbol]?.last, state.krakenData[symbol]?.last, state.whitebitData[symbol]?.last]
-        .map((last) => parseFloat(last))
-        .filter((last) => !isNaN(last));
-
-      const maxPrice = Math.max(...prices);
-      const minPrice = Math.min(...prices);
-
-      return {
-        name: symbol,
-        binance: state.binanceData[symbol]?.last || null,
-        kraken: state.krakenData[symbol]?.last || null,
-        whitebit: state.whitebitData[symbol]?.last || null,
-        priceDifference: prices.length > 1 ? maxPrice - minPrice : null,
-        priceDifferencePercentage: prices.length > 1 ? ((maxPrice - minPrice) / minPrice) * 100 : null,
-        minPrice,
-        maxPrice
-      };
-    });
-    setTableState(result);
-    setLoader(false);
-  }, [state]);
-
-  return loader ? (
-    <Box sx={{ width: "100%", height: "100%" }}>
-      <CircularProgress sx={{ fontSize: "150px" }} />
-    </Box>
-  ) : (
-    <>
-      <TableContainer>
-        <Table stickyHeader>
-          <TableHead>
-            <TableRow>
-              <TableCell>Pair name</TableCell>
-              <TableCell>Binance</TableCell>
-              <TableCell>Kraken</TableCell>
-              <TableCell>Whitebit</TableCell>
-              <TableCell>price Difference</TableCell>
-              <TableCell>Percentage difference</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {tableState
-              .sort((a: any, b: any) => b.priceDifferencePercentage - a.priceDifferencePercentage)
-              .map((item: any) => (
-                <TableRow>
-                  <TableCell>{item.name}</TableCell>
-                  <TableCell
-                    sx={{
-                      ...(item.maxPrice === item.binance && {
-                        background: "green !important",
-                        color: "white"
-                      }),
-                      ...(item.minPrice === item.binance && {
-                        background: "red !important",
-                        color: "white"
-                      })
-                    }}
-                  >
-                    {item.binance}
-                  </TableCell>
-                  <TableCell
-                    sx={{
-                      ...(item.maxPrice === item.kraken && {
-                        background: "green",
-                        color: "white"
-                      }),
-                      ...(item.minPrice === item.kraken && {
-                        background: "red",
-                        color: "white"
-                      })
-                    }}
-                  >
-                    {item.kraken}
-                  </TableCell>
-                  <TableCell
-                    sx={{
-                      ...(item.maxPrice === item.whitebit && {
-                        background: "green",
-                        color: "white"
-                      }),
-                      ...(item.minPrice === item.whitebit && {
-                        background: "red",
-                        color: "white"
-                      })
-                    }}
-                  >
-                    {item.whitebit}
-                  </TableCell>
-                  <TableCell>{item.priceDifference}</TableCell>
-                  <TableCell>{item.priceDifferencePercentage}</TableCell>
-                </TableRow>
-              ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </>
+  return (
+    <Stack sx={{ width: "500px", height: "100vh", background: "red" }}>
+      <Button onClick={() => TG.close()}>X</Button>
+      <Button onClick={() => console.log(TG)}>click</Button>
+    </Stack>
   );
 };
--- src/store/toolkitSlice.tsx
+++ src/store/toolkitSlice.tsx
@@ -1,25 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const toolkitSlice = createSlice({
-  name: 'toolkit',
-  initialState:{
-    count:0,
+  name: "toolkit",
+  initialState: {
+    count: 0,
     todos: ["qq", "Qweqwe", "qweqweq", "rvrvrv"]
   },
   reducers: {
     increment: (state) => {
-      state.count = state.count + 1
+      state.count = state.count + 1;
     },
     decrement: (state) => {
-      state.count = state.count - 1
+      state.count = state.count - 1;
     },
     addTodos: (state, action) => {
-      state.todos.push(action.payload)
+      state.todos.push(action.payload);
     },
     removeTodos: (state, action) => {
-      state.todos = state.todos.filter((todo) => todo !== action.payload)
+      state.todos = state.todos.filter((todo) => todo !== action.payload);
     }
-  },
+  }
 });
-export default toolkitSlice.reducer
-export const { increment, decrement, addTodos, removeTodos } = toolkitSlice.actions
+export default toolkitSlice.reducer;
+export const { increment, decrement, addTodos, removeTodos } = toolkitSlice.actions;
