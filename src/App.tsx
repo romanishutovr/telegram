6070b74bddbbd33917cd23578e8c4ab5fdf469ed
Roman <romanIshutov1995@gmail.com>
2024-07-11 19:45:18 +0300

    save

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { Button, Stack, Typography } from "@mui/material";
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection } from "firebase/firestore";
+import { doc, setDoc } from "firebase/firestore";
 
 declare global {
   interface Window {
@@ -10,13 +13,46 @@ declare global {
 const TG = window.Telegram.WebApp;
 
 export const App = () => {
+  const clientInitConfig = {
+    // apiKey: "AIzaSyBWCdZrnDtzHwWlieXGf0lmstzUEtKrZiM",
+    // authDomain: "test-1b4bd.firebaseapp.com",
+    // projectId: "test-1b4bd",
+    // storageBucket: "test-1b4bd.appspot.com",
+    // messagingSenderId: "904100009920",
+    // appId: "1:904100009920:web:e45388568e151df67fda25"
+    apiKey: "AIzaSyBWCdZrnDtzHwWlieXGf0lmstzUEtKrZiM",
+    authDomain: "test-1b4bd.firebaseapp.com",
+    projectId: "test-1b4bd",
+    storageBucket: "test-1b4bd.appspot.com",
+    messagingSenderId: "904100009920",
+    appId: "1:904100009920:web:e45388568e151df67fda25"
+  };
+
+  const firebaseApp = initializeApp(clientInitConfig);
+  const db = getFirestore(firebaseApp);
+
   useEffect(() => {
     TG.expand();
   }, []);
 
+  const addUser = useCallback(async () => {
+    const usersCollectionRef = collection(db, "users");
+
+    // Create a document reference with the custom ID
+    const customDocRef = doc(usersCollectionRef, "1"); // Use your desired ID here
+
+    // Set the document data, including the ID
+    await setDoc(customDocRef, {
+      id: "1",
+      name: "Los Angele11111"
+    });
+  }, [db]);
+
   return (
     <Stack sx={{ width: "100vh", height: "100vh" }}>
       <Button onClick={() => TG.close()}>X</Button>
+
+      <Button onClick={() => addUser()}>213123</Button>
       <Stack>
         <Typography>{TG?.initDataUnsafe?.user?.username}</Typography>
         <Typography>{TG?.initDataUnsafe?.user?.id}</Typography>
